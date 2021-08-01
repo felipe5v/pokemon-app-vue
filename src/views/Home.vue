a1b0503da5289163d8def5a1f5c9c0dad1c3e48b
felipe5v <luisfelipe432@hotmail.com>
2021-07-31 22:52:08 -0500

    first version using tailwind classes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,55 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="w-full flex justify-center">
+    <input
+      type="text"
+      placeholder="Enter Pokemon here"
+      class="mt-10 p-2 border-blue-500 border-2"
+      v-model="text"
+    />
+  </div>
+  <div class="mt-10 p-4 flex flex-wrap justify-center">
+    <div
+      class="ml-4 text-2xl text-blue-400"
+      v-for="(pokemon, idx) in filteredPokemon"
+      :key="idx"
+    >
+      <router-link :to="`/about/${urlIdLookup[pokemon.name]}`">
+        {{ pokemon.name }}
+      </router-link>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  import { reactive, toRefs } from "@vue/reactivity";
+  import { computed } from "@vue/runtime-core";
+  export default {
+    name: "Home",
+    setup() {
+      const state = reactive({
+        pokemons: [],
+        urlIdLookup: {},
+        text: "",
+        filteredPokemon: computed(() => updatePokemon()),
+      });
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+      const updatePokemon = () => {
+        if (!state.text) return [];
+        return state.pokemons.filter((pokemon) => {
+          return pokemon.name.includes(state.text);
+        });
+      };
+
+      const fetchPokemons = async () => {
+        const res = await fetch("https://pokeapi.co/api/v2/pokemon?offset=0");
+        const data = await res.json();
+        state.pokemons = data.results;
+        state.urlIdLookup = data.results.reduce((acc, cur, idx) => {
+          return (acc = { ...acc, [cur.name]: idx + 1 });
+        }, {});
+      };
+      fetchPokemons();
+      return { ...toRefs(state) };
+    },
+  };
 </script>
